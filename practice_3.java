df6bab47b6bc069ec4253a62095d6ad4bd27cf9d
Adil Khan <99200067+adilkhan2300@users.noreply.github.com>
2024-08-20 11:16:40 +0530

    Create practice_3.java

---
+++ practice_3.java
@@ -0,0 +1,15 @@
+//else if statement 
+public class Main{
+  public static void main(String[] args){
+    int x=3;
+    if(x<10){
+      System.out.println("then value is less then 10");}
+    else if(x>10){
+      System.out.println("then value is more than 10");}
+ 
+    else{
+        System.out.println("the value is something");
+    }
+    
+  }
+}
