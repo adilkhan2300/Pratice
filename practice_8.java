c0c9866905084a86b4a187af4f9d2ed453bd02ce
Adil Khan <99200067+adilkhan2300@users.noreply.github.com>
2024-08-21 15:09:37 +0530

    Create practice_8.java

---
+++ practice_8.java
@@ -0,0 +1,9 @@
+//using for loop  print even values between 0 and 10:
+public class Main{
+  public static void main(String[] args)
+  {
+for (int i = 0; i <= 10; i = i + 2) {
+  System.out.println(i);
+}
+  }
+}
