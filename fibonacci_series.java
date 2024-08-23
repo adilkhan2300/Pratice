cd527713a2c21aee774f0a32d669b044556c6577
Adil Khan <99200067+adilkhan2300@users.noreply.github.com>
2024-08-24 00:16:03 +0530

    Update fibonacci_series.java

---
+++ fibonacci_series.java
@@ -0,0 +1,20 @@
+// finding fibonacci series
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of terms: ");
+        int n = scanner.nextInt();
+        int a = 0, b = 1, next;
+        
+        System.out.print("Fibonacci Series: " + a + " " + b + " ");
+        
+        for (int i = 2; i < n; i++) {
+            next = a + b;
+            System.out.print(next + " ");
+            a = b;
+            b = next;
+        }
+    }
+}
